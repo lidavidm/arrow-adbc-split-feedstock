c85a76c0b34562e6d1c6e983a8b2ccc55f025510
David Li <li.davidm96@gmail.com>
2024-01-08 11:26:47 -0500

    try (4)

--- recipe/build-cpp.bat
+++ recipe/build-cpp.bat
@@ -1,27 +1,27 @@
 if "%PKG_NAME%" == "libadbc-driver-flightsql" (
-    set CMAKE_FLAGS=-DADBC_DRIVER_FLIGHTSQL=ON
-    goto BUILD
+    set DRIVER_BINARY=libadbc_driver_flightsql.dll
+    goto BUILDGO
 )
 if "%PKG_NAME%" == "libadbc-driver-manager" (
     set CMAKE_FLAGS=-DADBC_DRIVER_MANAGER=ON
-    goto BUILD
+    goto BUILDCPP
 )
 if "%PKG_NAME%" == "libadbc-driver-postgresql" (
     set CMAKE_FLAGS=-DADBC_DRIVER_POSTGRESQL=ON
-    goto BUILD
+    goto BUILDCPP
 )
 if "%PKG_NAME%" == "libadbc-driver-snowflake" (
-    set CMAKE_FLAGS=-DADBC_DRIVER_SNOWFLAKE=ON
-    goto BUILD
+    set DRIVER_BINARY=libadbc_driver_snowflake.dll
+    goto BUILDGO
 )
 if "%PKG_NAME%" == "libadbc-driver-sqlite" (
     set CMAKE_FLAGS=-DADBC_DRIVER_SQLITE=ON
-    goto BUILD
+    goto BUILDCPP
 )
 echo Unknown package %PKG_NAME%
 exit 1
 
-:BUILD
+:BUILDCPP
 
 mkdir "%SRC_DIR%"\build-cpp\%PKG_NAME%
 pushd "%SRC_DIR%"\build-cpp\%PKG_NAME%
@@ -38,5 +38,17 @@ cmake ..\..\c ^
 
 ninja -t targets
 cmake --build . --target install --config Release -j || exit /B 1
+
+goto END
+
+:BUILDGO
+
+pushd "%SRC_DIR%"\go\adbc\pkg
+
+make %DRIVER_BINARY%
+
+goto END
+
+:END
 
 popd
